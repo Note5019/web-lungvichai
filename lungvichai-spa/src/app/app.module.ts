f9cd4fd06fb06722ade6a37c6d7121831905f041
Note5019 <40689551+Note5019@users.noreply.github.com>
2019-10-20 21:17:54 +0700

    Add ngx & bootstrap

--- lungvichai-spa/src/app/app.module.ts
+++ lungvichai-spa/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppComponent } from './app.component';
 
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
