79caa124c39f2276d3b003ac020daf96bd84473d
Note5019 <40689551+Note5019@users.noreply.github.com>
2019-10-20 21:34:38 +0700

    add routing

--- lungvichai-spa/src/app/app.module.ts
+++ lungvichai-spa/src/app/app.module.ts
@@ -1,20 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { BsDropdownModule } from "ngx-bootstrap/dropdown";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { HomeComponent } from "./pages/home/home.component";
+import { HeaderComponent } from './layouts/header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent, HeaderComponent],
   imports: [
     BrowserModule,
+
+    //for http
     HttpClientModule,
+
+    //for routing
+    AppRoutingModule,
+
+    //for ngx style
     BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
